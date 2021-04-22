13e0c1349c78649edd060e276dc5d316bf232fd8
Bram Devries <bram@madewithlove.be>
2021-04-22 19:41:46 +0200

    Setup PHPUnit

---
+++ tests/SdkTest.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Client\Tests;
+
+final class SdkTest extends TestCase
+{
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Api\Client\Tests;
+
+abstract class TestCase extends \PHPUnit\Framework\TestCase
+{
+}
