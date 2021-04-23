94aee1d8323d9535aa934e8ab152f62162710fee
Bram Devries <bram@madewithlove.be>
2021-04-23 14:55:19 +0200

    Always use MockClient strategy during tests

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
 
+use Http\Discovery\ClassDiscovery;
+use Http\Discovery\Strategy\MockClientStrategy;
+
 require __DIR__.'/../vendor/autoload.php';
+
+ClassDiscovery::prependStrategy(MockClientStrategy::class);
