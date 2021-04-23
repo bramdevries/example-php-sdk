e7372a3f551fe499716e28b44c0d8c58ff3b95ef
Bram Devries <bram@madewithlove.be>
2021-04-23 14:55:32 +0200

    Assert response returns a 200 status code

--- tests/SdkTest.php
+++ tests/SdkTest.php
@@ -6,4 +6,11 @@ namespace Api\Client\Tests;
 
 final class SdkTest extends TestCase
 {
+    public function testCanRequest200Response(): void
+    {
+        $httpClient = $this->givenSdk()->getHttpClient();
+        $response = $httpClient->get('/todos');
+
+        $this->assertEquals(200, $response->getStatusCode());
+    }
 }
