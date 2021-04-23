547b3aa3c520353c10e344e2edbb466bb8dca58b
Bram Devries <bram@madewithlove.be>
2021-04-23 15:33:39 +0200

    Add test case for handling multiple 404 responses

--- tests/SdkTest.php
+++ tests/SdkTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Api\Client\Tests;
 
+use Http\Message\RequestMatcher\RequestMatcher;
 use Laminas\Diactoros\Response;
 
 final class SdkTest extends TestCase
@@ -35,5 +36,17 @@ final class SdkTest extends TestCase
 
         $this->assertEquals(201, $httpClient->post('/todos')->getStatusCode());
         $this->assertEquals(201, $httpClient->post('/todos')->getStatusCode());
+    }
+
+    public function testCanHandle404s(): void
+    {
+        $this->mockClient->on(new RequestMatcher('404'), (new Response())->withStatus(404));
+
+        $httpClient = $this->givenSdk()->getHttpClient();
+
+        $this->assertEquals(200, $httpClient->get('/todos')->getStatusCode());
+        $this->assertEquals(404, $httpClient->get('/404')->getStatusCode());
+        $this->assertEquals(404, $httpClient->get('/404')->getStatusCode());
+        $this->assertEquals(404, $httpClient->get('/404')->getStatusCode());
     }
 }
