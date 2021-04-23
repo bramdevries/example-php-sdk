585ea04bb3944cff4a79368dde8e80ca5222a475
Bram Devries <bram@madewithlove.be>
2021-04-23 15:14:14 +0200

    Add MockClient instance to base test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace Api\Client\Tests;
 
+use Api\Client\ClientBuilder;
+use Api\Client\Options;
 use Api\Client\Sdk;
+use Http\Mock\Client;
 
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    protected Client $mockClient;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->mockClient = new Client();
+    }
+
     protected function givenSdk(): Sdk
     {
-        return new Sdk();
+        return new Sdk(new Options([
+            'client_builder' => new ClientBuilder($this->mockClient),
+        ]));
     }
 }
