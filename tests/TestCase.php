6e961de19c5f407ce78f339530e23e3c7833835c
Bram Devries <bram@madewithlove.be>
2021-04-23 13:49:04 +0200

    Add givenSDK method to base test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Api\Client\Tests;
 
+use Api\Client\Sdk;
+
 abstract class TestCase extends \PHPUnit\Framework\TestCase
 {
+    protected function givenSdk(): Sdk
+    {
+        return new Sdk();
+    }
 }
